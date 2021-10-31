5e4c4efcb6c1c6e3b959ca62f3945417cbd4f651
Nimrod Flaschner <nimrodifla@gmail.com>
2021-10-31 23:00:37 +0200

    fix get

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/style.css', (req, res)=>{
+    res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/script.js', (req, res)=>{
+    res.sendFile(__dirname + '/script.js');
+});
+
 app.listen(process.env.PORT, (err)=>{
     if (err)
         throw err;
